cde8e6a2bc514ee3f0999412b8ff483f7196c961
RichardBongani <phalarichard1@gmail.com>
2020-07-20 23:06:04 +0200

    initial commit for dunamis

---
+++ application/script.js
@@ -0,0 +1,9 @@
+// This code is for the automated typing that is happening:
+var typed = new Typed('.element', {
+    strings: ["Trusted", "Committed", "Efficient", "Suppotive"],
+    loop:true,
+    typeSpeed: 100
+  });
+  
+
+  
